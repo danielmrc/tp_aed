420006e2eb8374dabec4191080dbe530f456328b
daniel <danielmoreira3397@gmail.com>
2022-04-30 21:27:54 -0300

    criacao de lista duplamente encadeada

---
+++ src/lista/Lista.java
@@ -0,0 +1,18 @@
+package lista;
+
+public class Lista {
+    private Elemento elemento;
+
+    public void inserir(){
+        
+    }
+
+    public void setElemento(Elemento novo){
+        this.elemento = novo;
+    }
+
+    public Elemento getElemento(){
+        return this.elemento;
+    }
+    
+}
