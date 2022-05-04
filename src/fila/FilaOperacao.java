cc0f8da91b1ae295cd93bd7d02e6034e47bb3e0f
daniel <danielmoreira3397@gmail.com>
2022-05-03 22:49:38 -0300

    ajustes finais antes da entrega, alterando o extrato

--- src/fila/FilaOperacao.java
+++ src/fila/FilaOperacao.java
@@ -35,12 +35,14 @@ public class FilaOperacao {
         System.out.println("------- Extrato -------");
         while(auxOperacao.getProximo() != null){
             if(auxOperacao.getProximo().getDado().getNumConta() == numConta){
-                if(auxOperacao.getProximo().getDado().getCodOperacao() == 1)
-                    System.out.println("Saque");
-                if(auxOperacao.getProximo().getDado().getCodOperacao() == 0)
-                    System.out.println("Depósito");
-                    else
-                        System.out.println("Código de operação desconhecido!!");
+                switch(auxOperacao.getProximo().getDado().getCodOperacao()){
+                    case 1: System.out.println("Saque");
+                    break;
+                    case 0: System.out.println("Depósito");
+                    break;
+                    default: System.out.println("Código de operação desconhecido!!");
+                    break;
+                }
                 System.out.println("Numero da conta: " + auxOperacao.getProximo().getDado().getNumConta());
                 System.out.println("Valor: " + auxOperacao.getProximo().getDado().getValor());
                 System.out.println("Data: " + auxOperacao.getProximo().getDado().getData().getData() + "\n\n");
